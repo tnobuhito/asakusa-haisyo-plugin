9dc50159a530f9a10857177f6b8aca060393327d
kumojima <kumojima@gmail.com>
2014-12-09 14:27:05 +0900

    * JSコードの埋め込みを防ぐため拝承メッセージをdata属性へ移動 * XSSを防ぐためhtml_escapeを追加

--- plugins/as_haisyo/lib/haisyo_listener.rb
+++ plugins/as_haisyo/lib/haisyo_listener.rb
@@ -3,14 +3,14 @@ class HaisyoListener < AsakusaSatellite::Hook::Listener
     message = truncate(context[:message].body.gsub(/\r\n|\r|\n|\t/," "))
 
 js_code = <<JSCODE
-$(function() {
-  $('#message').val('拝承。 ＞ #{message}');
+(function(message) {
+  $('#message').val('拝承。 ＞ ' + message + '');
   $('.inputarea').submit();
-});
+})($(this).data('haisyo'));
 JSCODE
 
     return <<HTML
-<img src="/plugin/as_haisyo/image/haisyo.png" onClick="#{js_code}" alt="拝承。">
+<img src="/plugin/as_haisyo/image/haisyo.png" data-haisyo="#{ERB::Util.html_escape message}" onClick="#{js_code}" alt="拝承。">
 HTML
   end
 end
